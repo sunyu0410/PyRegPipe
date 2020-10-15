996eb651e00e975b8c03764782706fba239d562c
Yu Sun <sunyu0410@gmail.com>
2020-10-15 13:22:10 +1100

    Updated the motion correction slices in dce_motion.py, hence it can be used on patients 46 onwards.

--- dce_motion.py
+++ dce_motion.py
@@ -37,4 +37,30 @@ corrt_slice =  {'mrhist005': '10',
                 'mrhist042': '15',
                 'mrhist043': '16',
                 'mrhist044': '25',
-                'mrhist045': '15'}
+                'mrhist046': '14', #
+            #   'mrhist047': None, # 14 was first chosen - introduced movement between 9-11 frames; correction result using frame 16 was worse - may be better off not using motion correction 
+            #   'mrhist048': '15', # same problem as 047, "corrected" one was worse; 3D motion correction on 15 worked better
+                'mrhist049': '14', #
+            #   'mrhist050': '17', # very bad motion, see if 3D is better, but otherwise may need to find another method
+                'mrhist051': '16', #
+                'mrhist052': '21', #
+                'mrhist053': '16', # (3D)
+                'mrhist054': '14', # (3D)
+                'mrhist055': '14', # (3D)
+                'mrhist056': '15', # (3D)
+                'mrhist057': '14', # (3D)
+                'mrhist058': '12', # (3D)
+                'mrhist059': '16', # (3D)
+                'mrhist060': '12', # (3D)
+                'mrhist061': '15', # (3D)
+                'mrhist062': '12', # (3D)
+                'mrhist063': '15', # (3D)
+                'mrhist064': '18', # (3D)
+                'mrhist065': '13', # (3D)
+                'mrhist066': '17', # (3D)
+                'mrhist067': '13', # (3D)
+                'mrhist068': '13', # (3D)
+                'mrhist069': '14', # (2D)
+                'mrhist070': '18'
+                }
+
